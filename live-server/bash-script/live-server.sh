2cb8176c3fb204160de7d1657f7302d21e084434
user-root <usersolo1@protonmail.com>
2026-07-15 02:08:34 +0000

    Change the path and add to the browser

--- live-server/bash-script/live-server.sh
+++ live-server/bash-script/live-server.sh
@@ -1,10 +1,10 @@
+
 #!/bin/bash
 
 FILE=$1
-DIR=$(dirname "$FILE")
-NAME=$(basename "$FILE")
 
- 
-brave "http://localhost:5001/$NAME" &
-exec node /home/snatbep/Dev/Projectos/Tools/live-server/http-server.js 5001 "$FILE" 
+current_directory=$(pwd)
+diferencia="${FILE#$current_directory}"
 
+brave "http://localhost:5001$diferencia" &
+exec node /home/snatbep/Dev/Projectos/Tools/live-server/http-server.js 5001 "$FILE"
